c529452b00b6e6c079f28b2b9000b900b05e84ae
mving <manuelvidal097@gmail.com>
2019-04-01 23:00:54 -0300

    .

--- 04-02/3.c
+++ 04-02/3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void eliminar_vocales(char *cad);
+void eliminar_vocales(char **cad);
 
 int main() {
 //  char cad[] = "hola";
@@ -9,26 +9,26 @@ int main() {
     printf("%s\n",vc[i]);
   }
   for (size_t i = 0; i < 3; i++) {
-    eliminar_vocales(vc[i]);
+    eliminar_vocales(&(vc[i]));
     printf("%s\n",vc[i]);
   }
 
   return 0;
 }
 
-void eliminar_vocales(char *cad){
+void eliminar_vocales(char **cad){
 
   int j = 0;
   printf("hh\n");
-  for (int i = 0; cad[i] != '\0' ; i++) {
+  for (int i = 0; *cad[i] != '\0' ; i++) {
     fprintf(stderr,"gg\n");
-    if (cad[i] != 'a' && cad[i] != 'e' && cad[i] != 'i' && cad[i] != 'o' && cad[i] != 'u') {
-      printf("%c\n",cad[3]);
-      cad[j]= cad[i];
+    if (*cad[i] != 'a' && *cad[i] != 'e' && *cad[i] != 'i' && *cad[i] != 'o' && *cad[i] != 'u') {
+      printf("%c\n",*cad[3]);
+      *cad[j]= *cad[i];
       fprintf(stderr,"gg\n");
       j++;
     }
   }
   fprintf(stderr,"gg\n");
-  cad[j] = '\0';
+  *cad[j] = '\0';
 }
